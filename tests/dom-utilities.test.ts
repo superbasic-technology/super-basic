d6326c9fa6ed0cb3cfbd25a06c74d80d62aeb51a
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-10 14:38:18 +0700

    test: add unit test for appendChildrentToParent function

---
+++ tests/dom-utilities.test.ts
@@ -0,0 +1,17 @@
+import { appendChildrenToParent } from "../src/utility/dom-utilities";
+
+describe("apprehendChildrenTOParent()", () => {
+  it("appends multiple children to the parent element", () => {
+    // Create a parent element
+    const parent = document.createElement("div");
+
+    // Create child elements
+    const child1 = document.createElement("h1");
+    const child2 = document.createElement("p");
+
+    appendChildrenToParent(parent, child1, child2);
+
+    expect(parent.children.length).toBe(2);
+    expect(parent.children[0]).toBe(child1);
+  });
+});
