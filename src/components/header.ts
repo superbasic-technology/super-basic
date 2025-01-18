fb61f0d5d70af39c99a0d840aa666f1236deaf1c
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-18 10:54:28 +0700

    feat: initialize HeaderLeft class implementation

--- src/components/header.ts
+++ src/components/header.ts
@@ -1,5 +1,20 @@
-export const header = () => {
-  const header = document.querySelector("header") as HTMLElement;
+import superbasic from "../assets/images/super-basic-technology.png";
+import menu from "../assets/images/menu.svg";
 
-  header.textContent = "Hello, world!";
+
+class HeaderLeft {
+  create(): void {
+    const header = document.querySelector("header") as HTMLElement;
+    const logo = document.createElement("div");
+    logo.setAttribute("class", "logo");
+    const img = document.createElement("img");
+    img.src = `${superbasic}`;
+
+    logo.appendChild(img);
+    header.appendChild(logo);
+  }
+}
+
+export const header = () => {
+  new HeaderLeft().create();
 };
