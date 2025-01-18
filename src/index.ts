1e45255d7076b13d73185f6aa2e7caf0df9981b6
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-18 15:38:58 +0700

    feat: call header() inside main()

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import "./styles/main.css"
 import { mainContent } from "./components/main-content";
+import { header } from "./components/header";
 
 const main = () => {
   mainContent();
+  header();
 };
 
 main();
