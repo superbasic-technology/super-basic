7909cf877ce1c4e2927d6316ac565b2823987709
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-18 15:59:39 +0700

    feat: initialize implementation of applyHandlers()

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import "./styles/main.css"
 import { mainContent } from "./components/main-content";
 import { header } from "./components/header";
+import { handleMenu } from "./utility/handlers";
 
 const main = () => {
   mainContent();
   header();
 };
 
+const applyHandlers = () => {
+  const menu = document.querySelector(".menu") as HTMLElement;
+  menu.addEventListener("click", handleMenu);
+};
+
 main();
+applyHandlers();
