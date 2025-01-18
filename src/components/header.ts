07359c786dae3f86d2cab2ca7f7b3b95032e2ada
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-18 16:48:41 +0700

    feat: initialize implementation of Navigation class

--- src/components/header.ts
+++ src/components/header.ts
@@ -1,5 +1,7 @@
 import superbasic from "../assets/images/super-basic-technology.png";
 import hamburger_menu from "../assets/images/menu.svg";
+import { appendChildrenToParent } from "../utility/dom-utilities";
+import cancel_icon from "../assets/images/cancel-icon.svg";
 
 
 class Logo {
@@ -29,6 +31,37 @@ class Menu {
   }
 }
 
+class Navigation {
+  create(): HTMLElement {
+    const nav = document.createElement("div");
+    nav.setAttribute("class", "nav");
+
+    const cancel = document.createElement("div");
+    cancel.setAttribute("class", "cancel");
+
+    const cancelIcon = document.createElement("img");
+    cancelIcon.src = `${cancel_icon}`;
+
+    cancel.appendChild(cancelIcon);
+    nav.appendChild(cancel);
+
+    const ul = document.createElement("ul");
+    const home = document.createElement("a");
+    home.textContent = "Home";
+
+    const about = document.createElement("a");
+    about.textContent = "About";
+
+    const contact = document.createElement("a");
+    contact.textContent = "Contact";
+
+    appendChildrenToParent(ul, about, home, contact);
+    nav.appendChild(ul);
+
+    return nav;
+  }
+}
+
 export class AppendToHeader {
   append(...elements: HTMLElement[]): void {
     const header = document.querySelector("header");
@@ -45,5 +78,6 @@ export const header = () => {
   new AppendToHeader().append(
     new Logo().create(),
     new Menu().create(),
+    new Navigation().create(),
   );
 };
