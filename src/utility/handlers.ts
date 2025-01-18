8488961f422e8f394cd40b652e877bc6a9b33214
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-18 16:47:12 +0700

    fix: update handleMenu(), so it hides .menu

--- src/utility/handlers.ts
+++ src/utility/handlers.ts
@@ -1,6 +1,10 @@
 export const handleMenu = () => {
+  const menu = document.querySelector(".menu") as HTMLElement;
+  menu.style.display = "none";
+
   const nav = document.querySelector(".nav") as HTMLElement;
   nav.style.display = "block";
+
 };
 
 export const handleCancel = () => {
