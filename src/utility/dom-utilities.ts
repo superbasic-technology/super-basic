9a7ddcfcd8bdb6fd0d6b35fd732148ce0fb318ab
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-19 17:42:04 +0700

    feat: initialize implementation of hideServices()

--- src/utility/dom-utilities.ts
+++ src/utility/dom-utilities.ts
@@ -3,4 +3,12 @@ export const appendChildrenToParent: AppendChildrenToParent = (parent, ...childr
   for (let i = 0; i < children.length; i += 1) {
     parent.appendChild(children[i]);
   }
+}
+
+export const hideServices = () => {
+  const cardsContainer = document.querySelector(".cards-container") as HTMLElement;  
+  cardsContainer.style.display = "none";
+
+  const servicesHeader = document.querySelector(".main-content > h2") as HTMLElement;
+  servicesHeader.style.display = "none";
 };
