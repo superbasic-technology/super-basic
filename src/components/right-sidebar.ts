f6ae0c866630e6852dce7e8f17015fe9a6c91668
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-20 09:01:21 +0700

    feat: initialize implementation of Internship class

--- src/components/right-sidebar.ts
+++ src/components/right-sidebar.ts
@@ -1,5 +1,28 @@
 import { appendChildrenToParent } from "../utility/dom-utilities";
 
-export const rightSidebar = () => {
+class Internship {
+  create(): HTMLElement {
+    const internship = document.createElement("div");
+    const internship_para = document.createElement("p");
+    internship_para.setAttribute("class", "internship");
+    internship_para.textContent = `
+       យើងខ្ញុំកំពុងត្រូវការអ្នកហាត់ការ (interns) ផ្នែក Frontend Development ចំនួន៥រូប។ សូមផ្ញើ CV ឬ ទាក់ទងមក Telegram ឬ facebook page របស់យើងខ្ញុំសម្រាប់ពត៌មានលម្អិត។
+    `;
 
+    internship.appendChild(internship_para);
+
+    return internship;
+  }
+}
+
+export const rightSidebar = () => {
+  const right_sidebar = document.querySelector(".sidebar.right") as HTMLElement;
+  const right_sidebar_h2 = document.createElement("h2");
+  right_sidebar_h2.textContent = "News";
+
+  appendChildrenToParent(
+    right_sidebar,
+    right_sidebar_h2,
+    new Internship().create(),
+    );
 };
