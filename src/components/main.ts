8c494cad2f95c7785da7e3d6553a7bc2b65150ac
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-23 13:55:59 +0700

    feat: style main > .home section

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,7 +1,4 @@
 import { appendChildrenToParent } from "../utility/dom-utilities";
-import { LeftSidebar } from "./left-sidebar";
-import { MainContent } from "./main-content";
-import { RightSidebar } from "./right-sidebar";
 import { Home } from "./home";
 
 export const main = () => {
