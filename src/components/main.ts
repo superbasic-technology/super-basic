74c31a4e32ea4ed29995a98d61b266686350157c
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-01-23 17:16:35 +0700

    feat: add .contact and .about to <main>

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,4 +1,6 @@
 import { appendChildrenToParent } from "../utility/dom-utilities";
+import { About } from "./about";
+import { Contact } from "./contact";
 import { Home } from "./home";
 
 export const main = () => {
@@ -7,5 +9,7 @@ export const main = () => {
   appendChildrenToParent(
     main,
     new Home().create(),
+    new Contact().create(),
+    new About().create(),
   );
 };
