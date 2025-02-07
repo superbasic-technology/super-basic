61b485ab4222065ddaf19945109a6227bba44495
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2025-02-07 20:37:12 +0700

    refactor: correct typos

--- src/components/left-sidebar.ts
+++ src/components/left-sidebar.ts
@@ -31,7 +31,7 @@ export class WhyNoInternshipOrJob {
     const whyNoInternshipOrJob_para = document.createElement("a");
     whyNoInternshipOrJob_para.setAttribute("class", "whyNoInternshipOrJob");
     whyNoInternshipOrJob_para.textContent = `
-      តើហេតុអ្វីសិស្សដែលរៀនជំនាញទាក់ទងនឹងការសរសេរកូដ«coding»មិនអាចរកឱកាសហាត់ការ«internship»ឬ​ឱកាសការងារបាន?
+      តើហេតុអ្វីសិស្សដែលរៀនជំនាញទាក់ទងនឹងការសរសេរកូដ«coding»មិនអាចរកឱកាសហាត់ការ«internship»ឬឱកាសការងារបាន?
       សូមទាក់ទងមកយើងខ្ញុំតាមរយៈTelegram ឬ facebook ខាងក្រោមដើម្បីទទួលបានប្រឹក្សាដោយឥតគិតថ្លៃ។
     `;
 
